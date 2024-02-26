99724056e69949faba7f62f9b6c8f3d527adada6
ImRaabe <raabees@mail.uc.edu>
2024-02-25 19:18:15 -0500

    Project init

--- src/CtoFandFtoC.java
+++ src/CtoFandFtoC.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class CtoFandFtoC {
-}
+import java.util.Scanner;
+
+public class CtoFandFtoC {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double temp;
+        char ForC; // For user to pick F or C
+        char again; // Do run program again with another temperature
+
+        do {
+            // Validate type of temperature
+            do {
+                System.out.println("Please enter the temperature type (F or C):");
+                ForC = Character.toUpperCase(in.next().charAt(0));
+            } while (ForC != 'F' && ForC != 'C');
+
+            // Validate input for temp
+            do {
+                System.out.println("Please enter the temperature value:");
+                while (!in.hasNextDouble()) {
+                    System.out.println("This temperature is invalid! Please enter a valid number:");
+                    in.next();
+                }
+                temp = in.nextDouble();
+            } while (temp < -273.15); // Absolute zero in Celsius
+
+            // Convert temperature based on F or C
+            if (ForC == 'F') {
+                temp = (temp - 32) * 5 / 9;
+                System.out.println("The temperature in Celsius is: " + temp + " C");
+            } else {
+                temp = (temp * 9 / 5) + 32;
+                System.out.println("The temperature in Fahrenheit is: " + temp + " F");
+            }
+
+            // Ask user if they want to convert another temperature
+            System.out.println("Do you want to convert another temperature? (Y/N)");
+            again = Character.toUpperCase(in.next().charAt(0));
+        } while (again == 'Y');
+
+        in.close();
+    }
+
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i <= 30; i++) {
+            System.out.println(i);
+        }
+        for (int i = 30; i >= 0; i--) {
+            System.out.println(i);
+        }
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.println(i);
+        }
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        int n = 7;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n / 2; j++) {
+                if (j == 0 || i == 0 || i == n / 2 || i == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
+
